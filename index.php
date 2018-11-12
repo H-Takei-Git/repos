6e6796a20ea8593324adf479c6b3281b23050aac
H Takei <hiro.zb3@gmail.com>
2018-11-12 15:05:26 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <from action="chat.php">
+        <input type="text" name="uname">
+        <button>Login</button>
+    </from>
+</body>
